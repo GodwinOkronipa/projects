28befaa2aa6f082bd303b5ab594c6e04cb0b12a4
godmode-dev <gokronipa@icloud.com>
2025-08-13 06:58:04 +0000

    Update simpleSmartContract.sol

--- simpleSmartContract.sol
+++ simpleSmartContract.sol
@@ -1,2 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+// This is our improved smart contract for storing a name.
+contract SimpleStorage {
+    // This state variable will store the owner's address.
+    address private owner;
+    
