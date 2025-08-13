44b5432fe63281d1a5386287f3e0b70d7c066626
godmode-dev <gokronipa@icloud.com>
2025-08-13 07:00:55 +0000

    Update simpleSmartContract.sol
    added a private state variable, owner, which stores the address of the person who deployed the contract;
    added an event called NameChanged that will be "emitted" every time the setName function is called, logging both the old and new names

--- simpleSmartContract.sol
+++ simpleSmartContract.sol
@@ -11,4 +11,26 @@ contract SimpleStorage {
     
     // This event will be emitted whenever the name is changed.
     event NameChanged(string oldName, string newName);
+
+    
+    // This is a special function called a constructor. It runs only once when the contract is deployed.
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    // This function sets the value of our name.
+    // The `onlyOwner` modifier ensures only the owner can call this function.
+    function setName(string memory newName) public onlyOwner {
+        string memory oldName = myName;
+        myName = newName;
+        // Emit the event to notify listeners of the change.
+        emit NameChanged(oldName, newName);
+    }
+    
+    // This is our custom modifier for access control.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
+}
     
